eb98c209e29465556015e2d5a742310929d4ce75
TtunMill <84768554+ttunmill@users.noreply.github.com>
2024-05-31 17:20:44 +0900

    4

--- js/index.js
+++ js/index.js
@@ -1,20 +1,37 @@
-let gnb = document.querySelector(".gnb")
+let gnb = document.querySelectorAll(".gnb > li")
 let nav_whith_bg = document.querySelector(".nav_whith_bg")
 let lnb = document.querySelectorAll(".lnb")
-gnb.addEventListener("mouseover", ()=> {
+/* gnb.addEventListener("mouseover", ()=> {
     nav_whith_bg.style.display = "block"
     for(var i=0; i<lnb.length; i++) {
         lnb[i].style.display = "block"
     }
     
+}) */
+for(var i of lnb) {i.style.height = "0px"}
+gnb.forEach((i, j) => {
+    i.addEventListener("mouseover", () => {
+        nav_whith_bg.style.height = "300px"
+        for(var i of lnb) {i.style.height = "225px"}
+    })
+    nav_whith_bg.addEventListener("mouseout", () => {
+        nav_whith_bg.style.height = "0px"
+        for(var i of lnb) {i.style.height = "0px"}
+    })
+    lnb[j].addEventListener("mouseout", () => {
+        nav_whith_bg.style.height = "0px"
+        for(var i of lnb) {i.style.height = "0px"}
+    })
 })
-nav_whith_bg.addEventListener("mouseout", ()=> {
-    nav_whith_bg.style.display = "none"
+
+
+/* nav_whith_bg.addEventListener("mouseout", ()=> {
+    
     for(var i=0; i<lnb.length; i++) {
         lnb[i].style.display = "none"
     }
 })
-
+ */
 
 //참고용
 /* gnb.forEach((obj, ind)=> {
